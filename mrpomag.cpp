23f5f8e7f4678ecfa2d66612e6c11fab29fe8691
Vasiliy Alchakov <v.alchakov@gmail.com>
2021-12-07 13:28:15 +0300

    Добавлена переменная c

--- mrpomag.cpp
+++ mrpomag.cpp
@@ -2,9 +2,10 @@
 
 int main()
 {
-    int a, b;
+    int a, b, c;
     a = 5;
     b = 2;
-    std::cout << a + b << std::endl;
+    c = a + b;
+    std::cout << c << std::endl;
     std::cout << "Hello World!\n";
 }
