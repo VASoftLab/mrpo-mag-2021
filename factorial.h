459754689876cc50444ea5262c5a2a09039f2f2b
saigon-89 <80712391+saigon-89@users.noreply.github.com>
2021-12-20 14:21:01 +0300

    Update factorial.h

--- factorial.h
+++ factorial.h
@@ -1,8 +1,8 @@
 #pragma once
 /**
  * \fn
- * \brief Функция взятия факториала от параметра
- * \param[in] n число для взятия факториала
- * \return Значение факториала числа n
+ * \brief Factorial function
+ * \param[in] n number to take factorial
+ * \return factorial of n
  */
 unsigned long long int factorial(unsigned int n);
